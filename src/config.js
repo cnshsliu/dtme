b70a27071a819eef92069767eae87447ea71c65f
Lucas Kehong Liu <kehong.liu@rootcloud.com>
2021-07-07 03:19:23 +0800

    src added

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+  backendUrl: 'https://localhost/api',
+  appmode: false,
+};
+export default config;
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import CONFIG from "./config";
+console.log("CONFIG.appmode:>", CONFIG.appmode, CONFIG.appmode ? 'will load serviceWorker' : 'will not load serviceWorker');
